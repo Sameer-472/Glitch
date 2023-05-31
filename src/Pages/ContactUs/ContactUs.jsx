4d03906f4441c4ace6091be5247184c97af6f1af
Sameer <muhammadsameer2016@yahoo.com>
2023-05-31 11:05:04 +0500

    latest commit

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -29,53 +29,64 @@ const ContactUs = () => {
       <div className="contact">
         <div className="contactPage">
           <div className="contactForm">
-            <h1 className="discussProject" >
+            <h1 className="discussProject">
               Ready to discuss <br /> Your Project with us ?{" "}
             </h1>
             <div className="upperInput">
-              <TextField
-                id="standard-basic"
-                variant="outlined"
-                label="Full Name"
-                InputProps={{ style: { color: "white"} }}
-                sx={{pt: 2}}                               
-              />
-              <TextField
-                id="standard-basic"
-                variant="outlined"
-                label="Email"
-                InputProps={{ style: { color: "white"} }}            
-                sx={{pt: 2}}
-              />
+              <div id="name">
+                <div style={{ textAlign: "start" }}>Full Name</div>
+                <input type="text" name="" id="" />
+              </div>
+              <div id="email">
+                <div style={{ textAlign: "start" }}>Email</div>
+                <input type="email" name="" id="" />
+              </div>
             </div>
-            <TextField
-              id="standard-basic"
-              fullWidth
-              variant="outlined"
-              label="About Project"
-              InputProps={{ style: { color: "white"} }}
-              sx={{pt: 2}}
-            />
-            <div className="lowerInput" >
-              <div onClick={handleButtonClick} className="attachedFile">
+            {/* <div id="about"> */}
+              <div id="about" style={{ textAlign: "start" }}>About Project</div>
+              <input type="email" name="" id="aboutProjectInput" />
+            {/* </div> */}
+            <div className="lowerInput">
+              <div onClick={handleButtonClick} className="attachedFile" id="attachedFile">
                 <input
                   ref={fileInputRef}
                   type="file"
                   style={{ display: "none" }}
                   onChange={handleFileSelection}
                 />
-                <AttachFileRounded />
-                <span>Attached a File</span>
+                <AttachFileRounded
+                  style={{
+                    border: "2px solid white",
+                    borderRadius: 50,
+                    padding: 5,
+                  }}
+                />
+                <span style={{ margin: 10, fontWeight: "bolder" }}>
+                  Attached a File
+                </span>
               </div>
-              {/* <Button variant="contained" className="submit">Send Response</Button> */}
-              <button className="submit">Submit</button>
+
+              <button className="submit">Send Message</button>
             </div>
-            
           </div>
           <div className="clientReview">
-            <img src="http://www.w3.org/2000/svg" alt="" />
             <h2 className="clientHeading">Our Client Say</h2>
-            <p className="review">“Halo Lab delivered a unique and original design that makes my company stand out from the competition — they didn’t provide a template-based design or something trendy. Their development is world-class and perfection itself.”</p>
+            <p className="review">
+              “Halo Lab delivered a unique and original design that makes my
+              company stand out from the competition — they didn’t provide a
+              template-based design or something trendy. Their development is
+              world-class and perfection itself.”
+            </p>
+            <div id="clientReviewBottom">
+              <img
+                src="https://www.halo-lab.com/cdn-cgi/image/anim=true,dpr=1,format=auto,quality=85/cloudflare-images/review-author.ba60a54903c.png?a717beb7ff7"
+                alt="hello"
+              />
+              <div id="companyName">
+                <div id="clientName">Rany</div>
+                <p id="clientCompany">Founder of casey</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
